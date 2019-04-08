2e9a3a90a79e2eb4dfddd67b5b54c35b8a9a8985
Robert Rong <robert.rong@agile-solutions.ch>
2019-04-08 14:02:35 +0200

    move to spring keycloak rest template

--- src/main/java/ch/agilesolutions/quarkus/ProductpageResource.java
+++ src/main/java/ch/agilesolutions/quarkus/ProductpageResource.java
@@ -7,6 +7,11 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.keycloak.adapters.springsecurity.client.KeycloakRestTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
 @Path("/productpage")
 public class ProductpageResource {
@@ -18,12 +23,35 @@ public class ProductpageResource {
 	@Inject
 	@RestClient
 	private DetailsServiceClient detailsService;
+	
+    @Value("${details.api.url}")
+    private String detailsApiUrl;
+	
+    @Value("${reviews.api.url}")
+    private String reviewsApiUrl;
+
+	private RestTemplate restTemplate;
+
+	private KeycloakRestTemplate keycloakRestTemplate;
+	
+    @Autowired
+    public ProductpageResource(RestTemplate restTemplate, KeycloakRestTemplate keycloakRestTemplate) {
+        this.restTemplate = restTemplate;
+        this.keycloakRestTemplate = keycloakRestTemplate;
+    }
+
 
 	@GET
 	@Produces(MediaType.TEXT_PLAIN)
 	public String hello() {
 
-		return String.format("Product with details : %s reviewed with : %s", detailsService.getDetails(),
-				reviewService.getReviews());
+		ResponseEntity<String> details = keycloakRestTemplate.getForEntity(detailsApiUrl, String.class);
+
+		ResponseEntity<String> reviews = keycloakRestTemplate.getForEntity(reviewsApiUrl, String.class);
+
+		
+		return String.format("Product with details : %s reviewed with : %s", details, reviews);
+//		return String.format("Product with details : %s reviewed with : %s", detailsService.getDetails(),
+//				reviewService.getReviews());
 	}
 }
